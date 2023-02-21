ae8a73c1937f7ccb9ca332a910f29a93c4a6d7e9
Rory Hendrickson <hendricksonrory@gmail.com>
2023-02-21 11:08:32 -0800

    change deps, add typescript

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,19 +1,11 @@
-let postCssConfig = {
+module.exports = {
   plugins: [
     'postcss-normalize',
-    'postcss-flexbugs-fixes',
     [
       'postcss-preset-env',
       {
         autoprefixer: true,
-        importFrom: ['src/global.css'],
-        features: {
-          'custom-media-queries': true,
-          'nesting-rules': true,
-        },
       },
     ],
   ],
 }
-
-module.exports = postCssConfig
